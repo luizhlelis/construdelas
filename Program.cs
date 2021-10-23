f3b209913bf04288b92bdf2d657629de9c77bf61
Luiz Lelis <luizhlelis@gmail.com>
2021-10-23 12:04:40 -0300

    chore: first monitory

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Ada.Filho;
+
+namespace Ada
+{
+    class Program
+    {
+        static void Main (String[] args)
+        {
+            Console.WriteLine("Calculadora");
+            Project.ChamaProjetoFilho();
+            var num1 = int.Parse(args[0]);
+            var num2 = int.Parse(args[1]);
+            var result = Multiplicar(num1, num2);
+            Console.WriteLine("Resultado: " + result);
+        }
+
+        static int Multiplicar(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
---
+++ filho/Project.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Ada.Filho
+{
+    class Project
+    {
+        public static void ChamaProjetoFilho ()
+        {
+            Console.WriteLine("Ada Filho");
+        }
+    }
+}
